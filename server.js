27544e071dc8f83ffe5f62d5197d567648c6394c
ihsanuddienullah <ihsanuddienullah@gmail.com>
2022-05-14 16:39:57 +0700

    fix cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const userdb = JSON.parse(fs.readFileSync("./users.json", "UTF-8"));
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
-server.use(jsonServer.defaults());
+server.use(jsonServer.defaults({noCors: false}));
 
 const SECRET_KEY = "123456789";
 
